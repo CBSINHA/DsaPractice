3b826c60a0dd3a0df5f83ecb1e104499117a972c
Shashank Sinha <shashanksinha590@gmail.com>
2025-07-01 18:33:45 +0530

    Used 2 pointer method following arrays sorted status

--- PairSum/OptimalSolution.cpp
+++ PairSum/OptimalSolution.cpp
@@ -14,12 +14,22 @@ int main(){
         cin>>x;
         array.push_back(x);
     }
-    //logic 
-    // Since the array is sorted, we know the first and last elements will be smallest and greatest respectively
+    int target;
+    cout<<"Enter the target: ";
+    cin>>target;
+    //logic (O(n))
+    // Since the array is SORTED, we know the first and last elements will be smallest and greatest respectively
     //we take the sum of first and last element and see:
     // if sum == target then those indices are the answer 
     //else if sum< target: we need to add a greater number in the sum and since nothing is greater than the last element of the array we add the 2nd element(2nd element> 1st)
     //else if sum>target: we added a larger number and have to reduce it so we move on the 2nd last element(smaller frm last element)
-    
+    int i=0, j=size-1;
+    while(i<j){
+        int sum;
+        sum=array.at(i)+array.at(j);
+        if(sum==target){cout<< i<< ","<<j;break;}
+        else if(sum<target)i++;
+        else j--;
+    }
     return 0;
 }
